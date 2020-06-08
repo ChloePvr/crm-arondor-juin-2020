d199e439672dac0bfd2eb47772eff563c0c695f6
Chloe Pouvreau <chloe.pouvreau@arondor.com>
2020-06-08 15:16:13 +0200

    modules generate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crm-juin-arondor';
+  public title = 'crm-juin-arondor';
+  constructor() {}
+  public sayHello() {
+    console.log('Hello girls :-) ')
+  }
 }
