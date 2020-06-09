c4504c81b4ae0c51feaf12417d2542b77005b111
Chloe Pouvreau <chloe.pouvreau@arondor.com>
2020-06-09 09:47:46 +0200

    ui and multiple ng-content

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavComponent, FooterComponent, HeaderComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [NavComponent, FooterComponent, HeaderComponent]
 })
 export class CoreModule { }
---
+++ src/app/ui/components/notice.md
@@ -0,0 +1,8 @@
+#how to use ?
+
+use 5 transclusions with ng-content
+need to project element with class="icon"
+need to project element with class="header"
+need to project element with class="nav"
+need to project element with class="content" for different pages which will be created dynamically by the rooter
+need to project element with class="footer"
