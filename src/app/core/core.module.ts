b5884cc320bdf8fa714228ca9ac667dd7abf027e
Chloe Pouvreau <chloe.pouvreau@arondor.com>
2020-06-10 17:01:18 +0200

    dynamic tables and pipes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,15 +4,16 @@ import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { TotalPipe } from './pipes/total.pipe';
 
 
 
 @NgModule({
-  declarations: [NavComponent, FooterComponent, HeaderComponent],
+  declarations: [NavComponent, FooterComponent, HeaderComponent, TotalPipe],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [NavComponent, FooterComponent, HeaderComponent]
+  exports: [NavComponent, FooterComponent, HeaderComponent, TotalPipe]
 })
 export class CoreModule { }
--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableComponent } from './components/table/table.component';
+import { BtnComponent } from './components/btn/btn.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TableComponent, BtnComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [TableComponent, BtnComponent]
 })
 export class LibraryModule { }
