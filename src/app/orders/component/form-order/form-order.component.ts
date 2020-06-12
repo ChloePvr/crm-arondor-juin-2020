6f4f555ac641b653908ac91046f18f77540c1950
Chloe Pouvreau <chloe.pouvreau@arondor.com>
2020-06-12 09:51:49 +0200

    Add orders + Validators and errors

--- src/app/orders/component/form-order/form-order.component.ts
+++ src/app/orders/component/form-order/form-order.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StateOrder } from 'src/app/core/enums/state-order.enum';
 import { Order } from 'src/app/core/models/order';
-import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-order',
@@ -12,6 +12,7 @@ export class FormOrderComponent implements OnInit {
   public states = Object.values(StateOrder);
   public form: FormGroup;
   @Input() initFormItem: Order;
+  @Output() submited: EventEmitter<Order> = new EventEmitter() ;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
@@ -20,11 +21,20 @@ export class FormOrderComponent implements OnInit {
       nbJours: [this.initFormItem.nbJours],
       tva: [this.initFormItem.tva],
       state: [this.initFormItem.state],
-      typePresta: [this.initFormItem.typePresta],
-      client: [this.initFormItem.client],
+      typePresta: [
+        this.initFormItem.typePresta,
+        Validators.required
+      ],
+      client: [
+        this.initFormItem.client,
+        [Validators.required, Validators.minLength(2)]
+       ],
       comment: [this.initFormItem.comment],
       id: [this.initFormItem.id]
     });
   }
 
+  public onSubmit() {
+    this.submited.emit(this.form.value);
+  }
 }
--- src/app/orders/pages/page-add-order/page-add-order.component.ts
+++ src/app/orders/pages/page-add-order/page-add-order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/core/models/order';
+import { OrdersService } from '../../services/orders.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-add-order',
@@ -8,9 +10,20 @@ import { Order } from 'src/app/core/models/order';
 })
 export class PageAddOrderComponent implements OnInit {
   public initFormItem = new Order();
-  constructor() { }
+  constructor(
+    private os : OrdersService,
+    private router : Router
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  public onSubmited(item: Order):void {
+    this.os.addItem(item).subscribe((res) => {
+      //redirection
+      this.router.navigate(['orders']);
+      //this.router.navigate(['orders','articles','test',item.id]);
+    });
   }
 
 }
